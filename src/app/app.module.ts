7d6e13ac575b3e458139d2904663801d3a81009a
martinorue <martin.orue008@gmail.com>
2021-11-09 17:22:20 -0300

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import 'angular-hammer';
 import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -19,7 +22,10 @@ import {MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule 
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
